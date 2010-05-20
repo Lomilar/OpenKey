de62d5d30bf6e8780d3a8a1efb7f253d397e0b11
rkeene <rkeene>
2010-05-20 20:36:01 +0000

    CACKey 0.5.2
    Updated to install scripts required by autoconf on release

--- build/build.sh
+++ build/build.sh
@@ -10,3 +10,15 @@ if [ ! -x configure ]; then
 fi
 
 rm -rf autom4te.cache vpn/nrlssc/vpngui/build/
+
+for basefile in install-sh config.sub config.guess; do
+	for path in /usr/share/automake-*; do
+		file="${path}/${basefile}"
+		if [ -f "${file}" ]; then
+			cp "${file}" .
+			chmod 755 "./${basefile}"
+
+			break
+		fi
+	done
+done
