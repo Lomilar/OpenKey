72ebff12831bf37b0e7d954d384496cb86b71ebb
rkeene <rkeene>
2010-07-21 21:33:18 +0000

    Updated to leave build directory intact on snapshot

--- build/build.sh
+++ build/build.sh
@@ -9,8 +9,6 @@ if [ ! -x configure ]; then
 	exit 1
 fi
 
-rm -rf autom4te.cache vpn/nrlssc/vpngui/build/
-
 for basefile in install-sh config.sub config.guess; do
 	for path in /usr/share/automake-*; do
 		file="${path}/${basefile}"
@@ -22,3 +20,9 @@ for basefile in install-sh config.sub config.guess; do
 		fi
 	done
 done
+
+if [ "${SNAPSHOT}" != "1" ]; then
+	mv build build_delete
+fi
+
+exit 0
