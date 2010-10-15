6560859cc525d2daa226067fb22493c2e422de1c
rkeene <rkeene>
2010-10-15 10:27:27 +0000

    Fixed issue with win32 build script
    Updated ignores

--- build/cackey_win32_build/build.sh
+++ build/cackey_win32_build/build.sh
@@ -1,1 +1,9 @@
-./configure --with-pcsc-headers=/home/u4423rsk/devel/pcsc_cac/cackey/build/cackey_win32_build/include --with-pcsc-libs=-L/home/u4423rsk/devel/pcsc_cac/cackey/build/cackey_win32_build/lib -lwinscard --host=i586-mingw32msvc  CPPFLAGS="-I/home/u4423rsk/devel/pcsc_cac/cackey/build/cackey_win32_build/include"
+#! /bin/bash
+
+make distclean
+
+./configure --with-pcsc-headers=/home/u4423rsk/devel/pcsc_cac/cackey/build/cackey_win32_build/include --with-pcsc-libs="-L/home/u4423rsk/devel/pcsc_cac/cackey/build/cackey_win32_build/lib -lwinscard" --host=i586-mingw32msvc  CPPFLAGS="-I/home/u4423rsk/devel/pcsc_cac/cackey/build/cackey_win32_build/include" || exit 1
+
+make || exit 1
+
+exit 0
