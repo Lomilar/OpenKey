81919038957a4e728ee7fbee10f2ee00b7ea7bc0
rkeene <rkeene>
2012-07-19 05:43:19 +0000

    Corrected name of new Windows compiler

--- build/cackey_win64_build/build.sh
+++ build/cackey_win64_build/build.sh
@@ -2,7 +2,7 @@
 
 make distclean
 
-./configure --with-pcsc-headers="$(pwd)/build/cackey_win64_build/include" --with-pcsc-libs="-L$(pwd)/build/cackey_win64_build/lib -lwinscard" --host=amd64-mingw32msvc CPPFLAGS="-I$(pwd)/build/cackey_win64_build/include" || exit 1
+./configure --with-pcsc-headers="$(pwd)/build/cackey_win64_build/include" --with-pcsc-libs="-L$(pwd)/build/cackey_win64_build/lib -lwinscard" --host=x86_64-w64-mingw32 CPPFLAGS="-I$(pwd)/build/cackey_win64_build/include" || exit 1
 
 make || exit 1
 
