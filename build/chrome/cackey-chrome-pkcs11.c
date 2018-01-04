ab7f7b4600ea41f822e69c146f63228071dad9c7
Ronald Ray <fritz.ray@eduworks.com>
2018-01-04 15:16:40 -0800

    Added internal function to perform rsa-oaep decrypt.

--- build/chrome/cackey-chrome-pkcs11.c
+++ build/chrome/cackey-chrome-pkcs11.c
@@ -644,6 +644,253 @@ cackey_chrome_returnType cackey_chrome_signMessage(struct cackey_certificate *ce
 	return(retval);
 }
 
+cackey_chrome_returnType cackey_chrome_decryptMessage(struct cackey_certificate *certificate, void *data, unsigned long dataLength, void *destination, unsigned long *destinationLength, char **pinPrompt, const char *pin) {
+	CK_RV chk_rv;
+	CK_ULONG numSlots, currSlot;
+	CK_SLOT_ID_PTR slots;
+	CK_SLOT_INFO slotInfo;
+	CK_SESSION_HANDLE hSession;
+	CK_OBJECT_HANDLE hObject, hKey;
+	CK_ULONG ulObjectCount;
+	CK_ATTRIBUTE searchTemplateCertificates[] = {
+		{CKA_CLASS, NULL, sizeof(CK_OBJECT_CLASS)},
+		{CKA_VALUE, NULL, 0}
+	};
+	CK_ATTRIBUTE searchTemplatePrivateKeys[] = {
+		{CKA_CLASS, NULL, sizeof(CK_OBJECT_CLASS)},
+		{CKA_ID, NULL, 0}
+	};
+	CK_ATTRIBUTE attrTemplateCertificate[] = {
+		{CKA_ID, NULL, 0},
+		{CKA_LABEL, NULL, 0}
+	};
+	CK_MECHANISM decryptMechanism = {CKM_RSA_PKCS_OAEP, NULL, 0};
+	CK_OBJECT_CLASS objectClassPrivateKey = CKO_PRIVATE_KEY;
+	CK_OBJECT_CLASS objectClassCertificate = CKO_CERTIFICATE;
+	CK_TOKEN_INFO tokenInfo;
+	CK_ULONG tmpDestinationLength;
+	char *certificateLabel;
+	int foundPrivateKeyObject;
+	cackey_chrome_returnType retval;
+
+	*pinPrompt = NULL;
+
+	retval = CACKEY_CHROME_ERROR;
+
+	chk_rv = cackey_chrome_init();
+	if (chk_rv != CKR_OK) {
+		return(retval);
+	}
+
+	chk_rv = moduleFunctionList->C_GetSlotList(FALSE, NULL, &numSlots);
+	if (chk_rv != CKR_OK) {
+		return(retval);
+	}
+
+	slots = malloc(sizeof(*slots) * numSlots);
+
+	chk_rv = moduleFunctionList->C_GetSlotList(FALSE, slots, &numSlots);
+	if (chk_rv != CKR_OK) {
+		free(slots);
+
+		return(retval);
+	}
+
+	searchTemplateCertificates[0].pValue = &objectClassCertificate;
+	searchTemplatePrivateKeys[0].pValue = &objectClassPrivateKey;
+
+	searchTemplateCertificates[1].pValue = certificate->certificate;
+	searchTemplateCertificates[1].ulValueLen = certificate->certificate_len;
+
+	foundPrivateKeyObject = 0;
+
+	certificateLabel = NULL;
+
+	for (currSlot = 0; currSlot < numSlots; currSlot++) {
+		chk_rv = moduleFunctionList->C_GetSlotInfo(slots[currSlot], &slotInfo);
+		if (chk_rv != CKR_OK) {
+			continue;
+		}
+
+		if ((slotInfo.flags & CKF_TOKEN_PRESENT) != CKF_TOKEN_PRESENT) {
+			continue;
+		}
+
+		chk_rv = moduleFunctionList->C_OpenSession(slots[currSlot], CKF_SERIAL_SESSION, NULL, NULL, &hSession);
+		if (chk_rv != CKR_OK) {
+			continue;
+		}
+
+		chk_rv = moduleFunctionList->C_FindObjectsInit(hSession, searchTemplateCertificates, sizeof(searchTemplateCertificates) / sizeof(searchTemplateCertificates[0]));
+		if (chk_rv != CKR_OK) {
+			moduleFunctionList->C_CloseSession(hSession);
+
+			continue;
+		}
+
+		while (1) {
+			chk_rv = moduleFunctionList->C_FindObjects(hSession, &hObject, 1, &ulObjectCount);
+			if (chk_rv != CKR_OK) {
+				break;
+			}
+
+			if (ulObjectCount == 0) {
+				break;
+			}
+
+			if (ulObjectCount != 1) {
+				break;
+			}
+
+			chk_rv = cackey_chrome_GetAttributesFromTemplate(hSession, hObject, attrTemplateCertificate, sizeof(attrTemplateCertificate) / sizeof(attrTemplateCertificate[0]));
+			if (chk_rv != CKR_OK) {
+				continue;
+			}
+
+			searchTemplatePrivateKeys[1].pValue = attrTemplateCertificate[0].pValue;
+			searchTemplatePrivateKeys[1].ulValueLen = attrTemplateCertificate[0].ulValueLen;
+
+			if (attrTemplateCertificate[1].ulValueLen > 0 && attrTemplateCertificate[1].pValue != NULL) {
+				certificateLabel = malloc(attrTemplateCertificate[1].ulValueLen + 1);
+				memcpy(certificateLabel, attrTemplateCertificate[1].pValue, attrTemplateCertificate[1].ulValueLen);
+				certificateLabel[attrTemplateCertificate[1].ulValueLen] = '\0';
+			}
+
+			break;
+		}
+
+		moduleFunctionList->C_FindObjectsFinal(hSession);
+
+		if (searchTemplatePrivateKeys[1].pValue != NULL) {
+			chk_rv = moduleFunctionList->C_FindObjectsInit(hSession, searchTemplateCertificates, sizeof(searchTemplateCertificates) / sizeof(searchTemplateCertificates[0]));
+			if (chk_rv == CKR_OK) {
+				while (1) {
+					chk_rv = moduleFunctionList->C_FindObjects(hSession, &hObject, 1, &ulObjectCount);
+					if (chk_rv != CKR_OK) {
+						break;
+					}
+
+					if (ulObjectCount == 0) {
+						break;
+					}
+
+					if (ulObjectCount != 1) {
+						break;
+					}
+
+					hKey = hObject;
+
+					foundPrivateKeyObject = 1;
+
+					break;
+				}
+
+				moduleFunctionList->C_FindObjectsFinal(hSession);
+			}
+
+			free(searchTemplatePrivateKeys[1].pValue);
+
+		}
+
+		if (foundPrivateKeyObject) {
+			chk_rv = moduleFunctionList->C_DecryptInit(hSession, &decryptMechanism, hKey);
+			if (chk_rv != CKR_OK) {
+				break;
+			}
+
+			tmpDestinationLength = *destinationLength;
+			chk_rv = moduleFunctionList->C_Decrypt(hSession, data, dataLength, destination, &tmpDestinationLength);
+			switch (chk_rv) {
+				case CKR_OK:
+					*destinationLength = tmpDestinationLength;
+					retval = CACKEY_CHROME_OK;
+					break;
+				case CKR_USER_NOT_LOGGED_IN:
+					chk_rv = moduleFunctionList->C_GetTokenInfo(slots[currSlot], &tokenInfo);
+					if (chk_rv == CKR_OK) {
+						if ((tokenInfo.flags & CKF_PROTECTED_AUTHENTICATION_PATH) == CKF_PROTECTED_AUTHENTICATION_PATH) {
+							retval = CACKEY_CHROME_NEEDPROTECTEDLOGIN;
+						} else {
+							retval = CACKEY_CHROME_NEEDLOGIN;
+
+							*pinPrompt = malloc(1024);
+							if (certificateLabel) {
+								snprintf(*pinPrompt, 1024, "Please enter the PIN for %s:%s", tokenInfo.label, certificateLabel);
+							} else {
+								snprintf(*pinPrompt, 1024, "Please enter the PIN for %s", tokenInfo.label);
+							}
+						}
+					} else {
+						retval = CACKEY_CHROME_NEEDLOGIN;
+
+						*pinPrompt = strdup("Please enter your Smartcard PIN");
+					}
+
+					if (retval == CACKEY_CHROME_NEEDPROTECTEDLOGIN) {
+						retval = CACKEY_CHROME_ERROR;
+
+						chk_rv = moduleFunctionList->C_Login(hSession, CKU_USER, NULL, 0);
+					} else {
+						if (pin) {
+							retval = CACKEY_CHROME_ERROR;
+
+							free(*pinPrompt);
+							*pinPrompt = NULL;
+
+							chk_rv = moduleFunctionList->C_Login(hSession, CKU_USER, (CK_UTF8CHAR_PTR) pin, strlen(pin));
+						} else {
+							chk_rv = CKR_GENERAL_ERROR;
+						}
+					}
+
+					if (chk_rv == CKR_OK && retval == CACKEY_CHROME_ERROR) {
+						chk_rv = moduleFunctionList->C_DecryptInit(hSession, &decryptMechanism, hKey);
+						if (chk_rv != CKR_OK) {
+							break;
+						}
+
+						tmpDestinationLength = *destinationLength;
+						chk_rv = moduleFunctionList->C_Decrypt(hSession, data, dataLength, destination, &tmpDestinationLength);
+
+						if (tmpDestinationLength == 0) {
+							chk_rv = CKR_GENERAL_ERROR;
+						}
+
+						switch (chk_rv) {
+							case CKR_OK:
+								*destinationLength = tmpDestinationLength;
+								retval = CACKEY_CHROME_OK;
+								break;
+							case CKR_USER_NOT_LOGGED_IN:
+								retval = CACKEY_CHROME_NEEDLOGIN;
+								break;
+							default:
+								retval = CACKEY_CHROME_ERROR;
+								break;
+						}
+					}
+
+					break;
+				default:
+					retval = CACKEY_CHROME_ERROR;
+					break;
+			}
+
+			break;
+		}
+
+		moduleFunctionList->C_CloseSession(hSession);
+	}
+
+	free(slots);
+
+	if (certificateLabel) {
+		free(certificateLabel);
+	}
+
+	return(retval);
+}
+
 #ifdef __cplusplus
 }
 #endif
